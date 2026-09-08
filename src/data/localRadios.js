7bac2b690bef4d3f32db7877458de78ffe985b9f
Ely Germano <elygermanogomes@gmail.com>
2026-09-08 20:53:54 -0300

    Adicionei o link do icone  do Hunter Pagode

--- src/data/localRadios.js
+++ src/data/localRadios.js
@@ -3,7 +3,7 @@ export const localRadios = [
     stationuuid: 'local-hunter-pagode',
     name: 'Hunter Pagode',
     url_resolved: 'https://live.hunter.fm/pagode_low',
-    favicon: '',
+    favicon: 'https://img.radios.com.br/radio/md/radio227987_1775563018.jpg',
     state: 'Brasil',
     country: 'Brasil',
     tags: 'pagode, samba, musica brasileira',
